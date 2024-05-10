cedf652c426156a8ecde5effdcffac33fa5458cc
gunavardhan12 <kothagattugunavardhan@gmail.com>
2024-05-10 10:55:50 +0530

    started another game

---
+++ Hollywood Tic Tac Toe/ViewControllers/Game2/GameVC.swift
@@ -0,0 +1,29 @@
+//
+//  GameVC.swift
+//  Hollywood Tic Tac Toe
+//
+//  Created by Wegile-Gunavardhan on 09/05/24.
+//
+
+import UIKit
+
+class GameVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

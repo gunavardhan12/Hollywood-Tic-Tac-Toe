cc7b4896af68ccb5ec5d2df86dd520646063e595
gunavardhan12 <kothagattugunavardhan@gmail.com>
2024-05-10 11:30:15 +0530

    ui of the game1 is done

--- Hollywood Tic Tac Toe/ViewControllers/Game2/GameVC.swift
+++ Hollywood Tic Tac Toe/ViewControllers/Game2/GameVC.swift
@@ -8,22 +8,46 @@
 import UIKit
 
 class GameVC: UIViewController {
-
+    var timer = Timer()
+    var totalTime = 30
+    @IBOutlet weak var numberLabel:UILabel!
+    @IBOutlet weak var timerLabel:UILabel!
+    @IBOutlet weak var a1:UIButton!
+    @IBOutlet weak var a2:UIButton!
+    @IBOutlet weak var a3:UIButton!
+    @IBOutlet weak var a4:UIButton!
+    @IBOutlet weak var b1:UIButton!
+    @IBOutlet weak var b2:UIButton!
+    @IBOutlet weak var b3:UIButton!
+    @IBOutlet weak var b4:UIButton!
+    @IBOutlet weak var c1:UIButton!
+    @IBOutlet weak var c2:UIButton!
+    @IBOutlet weak var c3:UIButton!
+    @IBOutlet weak var c4:UIButton!
+    @IBOutlet weak var d1:UIButton!
+    @IBOutlet weak var d2:UIButton!
+    @IBOutlet weak var d3:UIButton!
+    @IBOutlet weak var d4:UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(startTimer), userInfo: nil, repeats: true)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func startTimer(){
+        timerLabel.text = self.timeFormatted(self.totalTime)
+        if totalTime != 0{
+            totalTime -= 1
+        }
     }
-    */
+    func timeFormatted(_ totalSeconds: Int) -> String {
+        let seconds: Int = totalSeconds % 60
+        let minutes: Int = (totalSeconds / 60) % 60
+        return String(format: "%02d:%02d", minutes, seconds)
+    }
 
+}
+
+extension GameVC{
+    @IBAction func ButtonTapped(_ sender: UIButton){
+        sender.isHidden = true
+    }
 }
